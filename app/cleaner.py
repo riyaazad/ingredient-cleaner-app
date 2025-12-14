6f745fc3b323c2aae39d472f09763fc3a423aa69
riya <riyaazad@students.cs.ubc.ca>
2025-12-13 16:44:21 -0800

    added a cleander.py file which aims to clean all inputs texts for whitespace, common mix ups and concentration values

---
+++ app/cleaner.py
@@ -0,0 +1,30 @@
+import re #pythons regex
+
+ALIASES = {
+    "vitamin b-3": "niacinamide",
+    "vit b3": "niacinamide",
+    "octinoxate": "ethylhexyl methoxycinnamate",
+    "aqua": "water",
+    "parfum": "fragrance"
+    #we can keep adding more/extending this list for common mix ups
+}
+
+DO_NOT_MERGE = [
+    ("chloride", "chlorite")
+]
+
+def basic_clean(raw: str) -> str:
+    #this is to trim whitespace, lowercase, remove white space and clean string
+    cleaned = raw.strip().lower()
+    cleaned = cleaned.strip('"').strip("'")
+    cleaned = re.sub(r"\s+", " ", cleaned)
+    return cleaned
+
+def contains_concentration(text: str) -> bool:
+    return bool(re.search(r"\d+%", text)) #this is to check if we have a concentration (basically anything with %)
+
+def violates_do_not_merge(text: str) -> bool:
+    for a, b in DO_NOT_MERGE:
+        if a in text and b in text:
+            return True #true, dont merge
+    return False
